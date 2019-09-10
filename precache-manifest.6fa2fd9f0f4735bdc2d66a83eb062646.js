39923f8ebb4a55915656ea8c58ce3622ec767598
Guilherme <guilherme.ferreira28@outlook.com>
2019-09-10 12:18:13 +0200

    Updates

---
+++ precache-manifest.6fa2fd9f0f4735bdc2d66a83eb062646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6254604e638a683a90d8cd3cf16cd22e",
+    "url": "/DrumMachine/index.html"
+  },
+  {
+    "revision": "f1eae2afda9bf1069c4b",
+    "url": "/DrumMachine/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2044f84fd859efbb52a6",
+    "url": "/DrumMachine/static/js/2.9b3ecdf5.chunk.js"
+  },
+  {
+    "revision": "f1eae2afda9bf1069c4b",
+    "url": "/DrumMachine/static/js/main.1e861e85.chunk.js"
+  },
+  {
+    "revision": "0e8c219071bb8e4aae95",
+    "url": "/DrumMachine/static/js/runtime~main.4c86b840.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DrumMachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
